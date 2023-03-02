42ebee74cd3303125808371ce0f5558bacc43483
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-02 14:37:54 +0000

    Exercise 1 solved

---
+++ Argumentos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string item in args)
+            {
+               Console.WriteLine(item); 
+            }
+            
+        }
+    }
+}
