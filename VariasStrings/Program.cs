b6658710133fb002c08f893f53e0f662ffc8708f
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-02 23:50:54 +0000

    Exercise 5 solved

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,18 +6,34 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            int x = 14;
+            int y = 21;
+            int z = 2319;
 
             string s = "Miguel";
             string n = @"\This in literally between backslashes\";
             string m = "The following unicode will show: \u26a1 ";
             string b = @"The following unicode won't show: \u26a1";
-            string c = "This tab\t spaces out but this tab \\t shows up";
+            string c = "This tab\t spaces out but this tab \\t shows up\n";
+
+            
             
             Console.WriteLine(s);
             Console.WriteLine(n);
             Console.WriteLine(m);
             Console.WriteLine(b);
             Console.WriteLine(c);
+            
+            string conc = s + " Feliciano";
+            string nonc = "This is exercise number " + 5;
+            string inter = $"{x} plus {y} equals {x + y}";
+            string format = String.Format("X is {0}, Y is {1}", x,y);
+            Console.WriteLine("The value of z is :{0}", z);
+
+            Console.WriteLine(conc);
+            Console.WriteLine(nonc);
+            Console.WriteLine(inter);
+            Console.WriteLine(format);
         }
     }
 }
