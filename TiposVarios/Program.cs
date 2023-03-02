9eebbb31a03a6073d1686e44ddef1b33cdef6796
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-02 22:37:12 +0000

    Created and printed all variables form the first part of Exercise 3

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double d = 14.21;
+            float f = 123.456f;
+            decimal z = 789.34m;
+            int i = 21;
+            uint u = 14U;
+            long l = 420L;
+            ulong n = 2319UL;
+            
+            
+
+            Console.WriteLine(d);
+            Console.WriteLine(f);
+            Console.WriteLine(z);
+            Console.WriteLine(i);
+            Console.WriteLine(u);
+            Console.WriteLine(l);
+            Console.WriteLine(n);
+
+        }
+    }
+}
