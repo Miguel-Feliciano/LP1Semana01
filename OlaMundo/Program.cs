c8c6ee52408a52ba9216e8d01977fb3b19210d05
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-02 13:01:26 +0000

    Add bye world

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -6,7 +6,8 @@ namespace OlaMundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye World!");
         }
     }
 }
