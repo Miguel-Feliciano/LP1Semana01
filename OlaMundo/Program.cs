cfb86ea7dd1d5d3cd6b28f38f81b97f234d83264
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-02 14:52:16 +0000

    Exercise 2 solved (Added comment)

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -4,6 +4,10 @@ namespace OlaMundo
 {
     class Program
     {
+        /// <summary>
+        /// Prints "Hello World!" and "Bye World!" in the console
+        /// </summary>
+        /// <param name="args">It is an array of strings</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
