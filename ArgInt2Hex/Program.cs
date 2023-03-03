50806544a1e3938dc1426bc0d59209becea3e7a6
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-03 11:23:52 +0000

    Exercise 7 part1 solved

---
+++ ArgInt2Hex/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ArgInt2Hex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool i = false;
+            int m;
+            foreach (var item in args)
+            {
+                i = int.TryParse(item, out m);
+                if (i)
+                {
+                     Console.WriteLine($"{m}-> É inteiro, valor hexadecimal: {m:x}");
+                }
+                else
+                {
+                    Console.WriteLine(item + "-> Não é inteiro");
+                }
+            }
+                
+        }
+    }
+}
